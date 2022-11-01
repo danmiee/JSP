5358fd80a24e2a2f9052bfcaec81200af30d86b6
danmiee <dan-mi@kakao.com>
2022-11-01 14:15:49 +0900

    모델1 방식의 회원제 게시판 만들기

--- MustHaveJSP/src/main/java/common/JDBConnect.java
+++ MustHaveJSP/src/main/java/common/JDBConnect.java
@@ -21,11 +21,11 @@ public class JDBConnect {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             // DB에 연결
-            String url = "jdbc:mysql://localhost:3306/musthave";  
+            String url = "jdbc:mysql://localhost:3306/musthave";
             String id = "musthave";
             String pwd = "tiger"; 
             con = DriverManager.getConnection(url, id, pwd); 
-
+        	
             System.out.println("DB 연결 성공(기본 생성자)");
         }
         catch (Exception e) {
@@ -33,22 +33,19 @@ public class JDBConnect {
         }
     }
 
-    // 두 번째 생성자
+	
+	  // 두 번째 생성자
     public JDBConnect(String driver, String url, String id, String pwd) {
-        try {
-            // JDBC 드라이버 로드
-            Class.forName(driver);  
-
-            // DB에 연결
-            con = DriverManager.getConnection(url, id, pwd);
-
-            System.out.println("DB 연결 성공(인수 생성자 1)");
-        }
-        catch (Exception e) {            
-            e.printStackTrace();
-        }
-    }
-
+    	try { 
+		  // JDBC 드라이버 로드
+		  Class.forName(driver);
+	  
+		  // DB에 연결
+		  con = DriverManager.getConnection(url, id, pwd);
+	  
+	  System.out.println("DB 연결 성공(인수 생성자 1)"); } catch (Exception e) {
+	  e.printStackTrace(); } }
+    
     // 세 번째 생성자
     public JDBConnect(ServletContext application) {
         try {
@@ -60,8 +57,8 @@ public class JDBConnect {
             String url = application.getInitParameter("MySQLURL"); 
             String id = application.getInitParameter("MySQLId");
             String pwd = application.getInitParameter("MySQLPwd");
-            con = DriverManager.getConnection(url, id, pwd);
-
+        	con = DriverManager.getConnection(url, id, pwd);
+            
             System.out.println("DB 연결 성공(인수 생성자 2)"); 
         }
         catch (Exception e) {
@@ -75,7 +72,7 @@ public class JDBConnect {
             if (rs != null) rs.close(); 
             if (stmt != null) stmt.close();
             if (psmt != null) psmt.close();
-            if (con != null) con.close(); 
+            if (con != null) con.close();
 
             System.out.println("JDBC 자원 해제");
         }
